826d577bcb95fad47c27a8e6be372a317b123fc5
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2025-01-23 22:24:02 +0000

    Week 21 checkBrackets complete. Need to do Remove Pairs of Adjacent Letters Homework.

---
+++ src/Lesson21HW/Main.java
@@ -0,0 +1,44 @@
+package Lesson21HW;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkBrackets("()()()[]{{[]}}"));
+        System.out.println(checkBrackets("((])[)"));
+        System.out.println(checkBrackets("((({}{[]})))"));
+        System.out.println(checkBrackets(")("));
+        System.out.println(checkBrackets(""));
+    }
+
+    public static String removePairsOfAdjacentLetters(String string){
+        return "";
+    }
+
+    public static boolean checkBrackets(String input){
+        if (input.length() < 1 || input.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> brackets = new HashMap<>();
+        brackets.put('}','{');
+        brackets.put(']','[');
+        brackets.put(')','(');
+
+        for(char c : input.toCharArray()) {
+            if(brackets.values().contains(c)) {
+                stack.push(c);
+            } else {
+                if(stack.empty()) return false;
+                if(stack.peek() == brackets.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
